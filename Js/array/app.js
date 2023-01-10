2a4357f38cfc60c5070dc2e79941129277cb551c
MOHAMMED <momotaha039@gmail.com>
2023-01-10 15:50:03 +0200

    array

--- Js/array/app.js
+++ Js/array/app.js
@@ -21,8 +21,12 @@ copy=people.slice(2)
 
 //8
 console.log(people.indexOf("Mary"));
+//9
 console.log(people.indexOf("ds"));
-
+//10
 people = ["Greg", "Mary", "Devon", "James"];
-
-people=people.splice()
+people.splice(2,1,"Elizabeth","Artie")
+console.log(people)
+//11
+const withBob=people.concat("Bob")
+console.log(withBob)
