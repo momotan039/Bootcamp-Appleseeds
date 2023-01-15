6d36969f453332e68fc75baf4e017e461a8d83f4
MOHAMMED <momotaha039@gmail.com>
2023-01-15 10:37:53 +0200

    last

--- Js/while loop/app.js
+++ Js/while loop/app.js
@@ -7,5 +7,6 @@ const listOfNeighbours = [
 for (let i = 0; i < listOfNeighbours.length; i++) {
     for (let j = 0; j < listOfNeighbours[i].length; j++) {
         console.log("Neighbour:",listOfNeighbours[i][j])
+        
     }    
 }
