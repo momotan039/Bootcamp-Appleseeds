c21de5c722a01fe4beaa4116ab8894f1226c211f
momotan <momotaha039@gmail.com>
2023-01-19 11:16:15 +0200

    ex2-1

--- Js/Weekend Exercises 1/app.js
+++ Js/Weekend Exercises 1/app.js
@@ -1,1 +1,16 @@
 ex1=(isYes)=>isYes?"yes":"false";
+function ex2_1(arr){
+    // get smallest num
+    const min1=Math.min(...arr)
+    const iMin1=arr.indexOf(min1)
+    // remove smalleset num
+    arr.splice(iMin1,iMin1+1)
+    // get another smallest num
+    const min2=Math.min(...arr)
+    return min1+min2;
+}
+console.log(ex2_1([1,2,3,4]));
+
+function ex2_2(){
+    
+}
