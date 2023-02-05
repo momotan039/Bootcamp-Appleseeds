04a534f42b2e2115076df1e8f5477f7452f7a745
momotan <momotaha039@gmail.com>
2023-02-05 16:20:16 +0200

    fix navigation and kills section

--- protfolio/main.js
+++ protfolio/main.js
@@ -1,15 +1,47 @@
+let selectedItem
+
 window.onload=()=>{
+    selectedItem=document.querySelector('#main-menu ul a') 
     const offsetSkill=document.querySelector("#skills").offsetHeight
     window.onscroll=()=>{
         if(window.scrollY>offsetSkill)
             startSkillAnimation()
+            
     }
+
+    selectItemFromMenu()
+    handelArrowButton()
 }
 
 function startSkillAnimation(){
     const spans=document.querySelectorAll("#skills .skill")
     spans.forEach(s=>{
-        s.querySelector(".percent").style.width="100%"
-        s.querySelector(".percent").style.background="red"
+        s.classList.add('fill')
+    })
+}
+
+function selectItemFromMenu(){
+    const menu=document.querySelector('#main-menu ul')   
+    debugger 
+    selectedItem.classList.add('selected')
+    menu.childNodes.forEach(item=>{
+        item.addEventListener('click',()=>{
+            debugger
+            selectedItem.classList.remove('selected')
+            item.classList.add('selected')
+            selectedItem=item
+        })
+    })
+}
+
+function handelArrowButton(){
+    document.querySelectorAll('.arrowsLink').forEach(arrow=>{
+        arrow.addEventListener('click',()=>{
+           const idContainer=arrow.getAttribute('href')
+          const itemMenu=document.querySelector(`nav ul a[href='${idContainer}']`)
+          selectedItem.classList.remove('selected')
+          itemMenu.classList.add('selected')
+          selectedItem=itemMenu
+        })
     })
 }
