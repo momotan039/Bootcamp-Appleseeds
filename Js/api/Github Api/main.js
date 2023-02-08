8844617a0ddb3eee77d786f30693d794f125d840
momotan <momotaha039@gmail.com>
2023-02-08 11:24:22 +0200

    github api

---
+++ Js/api/Github Api/main.js
@@ -0,0 +1,59 @@
+const input=document.getElementsByTagName('input')[0]
+const users=[]
+const usersElm=document.getElementsByClassName('users')[0]
+
+ async function fetchUser(){
+    const userName=input.value
+    debugger
+    if(userName==="")
+    {
+        alert("Please Enter a username")
+        return
+    }
+     await fetch('https://api.github.com/users/'+userName)
+    .then((res)=>res.json()).then(user=>{
+        if('message' in user)
+        {
+            alert("This User Not Found")
+        }
+        else{
+            if(users.find(u=>u.name===user.name))
+            alert("This Uesr Already Exist Here")
+            else{
+                users.push(user)
+                usersElm.appendChild(createUesrCard(user))
+            }
+        }
+        input.value=""
+        input.focus()
+    })
+}
+function addUserToPage(user){
+    
+
+}
+function createUesrCard(user){
+    //creates elements
+    const card=document.createElement('div')
+    const img=document.createElement('img')
+    const h1=document.createElement('h1')
+    const h2=document.createElement('h2')
+    const a=document.createElement('a')
+    //configure elemnts
+    card.classList.add('card')
+    img.src=user.avatar_url
+    h1.innerText=user.name
+    h2.innerText=user.public_repos+" public repo"
+    a.href=user.html_url
+    a.target='_blank'
+    //apend childs
+    card.append(img,h1,h2,a)
+    return card
+}
+
+// document.addEventListener("keypress", async ()=>{
+//     debugger
+//    if(event.charCode===13)
+//    console.log("sd");
+// //    await fetchUser()
+// })
